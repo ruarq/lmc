a8f94e0d333b616b29083a82f7e0674a499c2d5b
ruarq <anton@green-pr.org>
2022-02-11 01:08:18 +0100

    fixed num_files calculation

--- format.sh
+++ format.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 files=$(find src -type f -regex ".*\..pp")
-num_files=${#files[@]}
+num_files=$(echo "$files" | wc -l)
 i=1
 for file in $files
 do
 	echo "[$i/$num_files] clang-format : $file"
 	clang-format -i $file
-	i+=1
+	i=$((i+1))
 done
